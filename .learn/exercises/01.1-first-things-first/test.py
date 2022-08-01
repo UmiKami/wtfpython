e95c0008228f32763f2f8bea622df33727906eff
ErnestoXG <ernestox.061@gmail.com>
2022-08-01 05:47:22 +0000

    fixed attribute erroor names

--- .learn/exercises/01.1-first-things-first/test.py
+++ .learn/exercises/01.1-first-things-first/test.py
@@ -6,28 +6,28 @@ def test_function_exists(app):
     try:
         assert app.some_func
     except AttributeError:
-        raise AttributeError('The function "myFunction" should exists')
+        raise AttributeError('The function "some_func" should exists')
 
 @pytest.mark.it('The function "some_func" should return the integer value 5')
 def test_function_returns_specified(app):
     try:
         assert app.some_func() == 5
     except AttributeError:
-        raise AttributeError('The function "myFunction" should exists')
+        raise AttributeError('The function "some_func" should return 5')
 
 @pytest.mark.it('You should create a variable named a')
 def test_variable_exists(app):
     try:
         assert app.a
     except AttributeError:
-        raise AttributeError('The variable "variables_are_cool" should exist on app.py')
+        raise AttributeError('The variable "a should exist on app.py')
 
 @pytest.mark.it('Variable a should be equal to 5')
 def test_variable_equals_5(app):
     try:
         assert app.a == 5
     except AttributeError:
-        raise AttributeError('The variable "variables_are_cool" should exist on app.py')
+        raise AttributeError('The variable "a" equal 5')
 
 @pytest.mark.it('The printed value on the console should be integer 5')
 def test_for_file_output(capsys, app):
